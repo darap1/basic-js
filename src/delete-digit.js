4d22f7c1e3f164a35dcb36b8b434893acb1fffb9
darap1 <darap@mail.ru>
2022-05-03 01:41:22 +0200

    update

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,8 +11,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+  let max = 0;
+    for (let i = 0; i<String(n).length; i++) {
+       let rez = String(n).split('').filter((a, index) => index!=i).join('')
+      max < +rez ? max = +rez : ''
+    }
+  return max
+ // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
 
--- src/what-season.js
+++ src/what-season.js
@@ -11,10 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSeason(date) {
+let month = '';
+  if (!date) {
+    return 'Unable to determine the time of year!';
+  }
+  try {
+    month = date.getMonth()
+  } catch {
+      throw new Error('Invalid date!')
+  }
+  
+  if (month < 2 || month === 11) return 'winter'
+  if (month < 5 && month > 1) return 'spring'
+  if (month < 8 && month > 4) return 'summer'
+  if (month < 11 && month > 7) return 'autumn'
 }
+
+
 
 module.exports = {
   getSeason
