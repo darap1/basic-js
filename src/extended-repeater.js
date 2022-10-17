92ecdc1b0eac88ecbb576be27f1937ab19ad290b
darap1 <darap@mail.ru>
2022-10-18 01:21:19 +0300

    feat: update tasks

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,25 +1,53 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Create a repeating string based on the given parameters
- *  
+ *
  * @param {String} str string to repeat
- * @param {Object} options options object 
+ * @param {Object} options options object
  * @return {String} repeating string
- * 
+ *
  *
  * @example
- * 
- * repeater('STRING', { repeatTimes: 3, separator: '**', 
+ *
+ * repeater('STRING', { repeatTimes: 3, separator: '**',
  * addition: 'PLUS', additionRepeatTimes: 3, additionSeparator: '00' })
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function repeater(str, options) {
+	let addArr = [];
+	let newStr = "";
+
+	if (options.addition === undefined) {
+		newStr += str;
+	} else {
+		if (
+			options.additionRepeatTimes === undefined ||
+			options.additionRepeatTimes === 1
+		) {
+			newStr = newStr + str + options.addition;
+		} else {
+			for (let i = 0; i < options.additionRepeatTimes; i++) {
+				addArr.push("" + options.addition);
+			}
+			newStr =
+				newStr + str + addArr.join("" + (options.additionSeparator || "|"));
+		}
+	}
+
+	if (options.repeatTimes === undefined || options.repeatTimes === 1) {
+		return newStr;
+	} else {
+		let newArr = [];
+		for (let i = 0; i < options.repeatTimes; i++) {
+			newArr.push(newStr);
+		}
+		return newArr.join("" + (options.separator || "+"));
+	}
 }
 
 module.exports = {
-  repeater
+	repeater,
 };
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,32 +1,45 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 const chainMaker = {
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+	chain: [],
+	getLength() {
+		return this.chain.length;
+	},
+	addLink(value) {
+		let newLink = "( " + value + " )";
+		this.chain.push(newLink);
+		return this;
+	},
+	removeLink(position) {
+		if (
+			position < 1 ||
+			position > this.getLength() ||
+			!Number.isInteger(position)
+		) {
+			this.chain = [];
+			throw new Error("You can't remove incorrect link!");
+		} else {
+			this.chain = this.chain.filter(
+				(value) => value !== this.chain[position - 1]
+			);
+			return this;
+		}
+	},
+	reverseChain() {
+		this.chain.reverse();
+		return this;
+	},
+	finishChain() {
+		let result = this.chain.join("~~");
+		this.chain = [];
+		return result;
+	},
 };
 
 module.exports = {
-  chainMaker
+	chainMaker,
 };
